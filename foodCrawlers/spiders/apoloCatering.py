a03dbe183028eae65d95ef7567427a27681caea6
alexalexroro <gurzou.alexandru@gmail.com>
2014-04-03 20:30:32 +0300

    Added a global header which can be used in any spider class

--- foodCrawlers/spiders/apoloCatering.py
+++ foodCrawlers/spiders/apoloCatering.py
@@ -1,16 +1,4 @@
-from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.item import Item, Field
-
-from foodCrawlers.items import FoodcrawlersItem
-from foodCrawlers.utils import checkAndExtract
-from foodCrawlers.utils import unicodeToAscii
-from foodCrawlers.foodItem import foodItem
-
-import unicodedata
-import string
-
+from spiderHeader import *
 items_per_row = 3
 
 class ApolloBananaCatering(CrawlSpider):	
